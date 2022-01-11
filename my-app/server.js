1e1fcb1dc5c5289e6dde69d49c9a89230469c92c
legend <kahramaninalcik@gmail.com>
2022-01-11 04:23:59 +0300

    first commit

--- my-app/server.js
+++ my-app/server.js
@@ -2,10 +2,12 @@ const cron = require('node-cron');
 const axios = require('axios')
 
 var begining = 3;
+var counter = 0;
 cron.schedule('*/5 * * * * *', function() {
   console.log('running a task every minute');
 
  begining = begining + Math.floor(Math.random() * 10);
+ counter = counter + 1;
 
 axios
   .post('http://52.164.121.193:8080/api/v1/Ci5C4o9SIefLterZE9UO/telemetry', {
@@ -23,7 +25,8 @@ axios
   axios
   .post('http://52.164.121.193:8080/api/v1/GCTSSq5w6u61dz9j2Sad/telemetry', {
     consumption: begining,
-    gas:Math.floor(Math.random() * 15)
+    gas:Math.floor(Math.random() * 15),
+    productCounter:counter
   })
   .then(res => {
     console.log(`statusCode: ${res.status}`)
